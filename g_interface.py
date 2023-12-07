48bc1812ddc0b795a828d29037a33e87f521ae94
shmyrevaaa <annashmyreva1805@gmail.com>
2023-12-07 17:47:06 +0700

    version 07-12

---
+++ g_interface.py
@@ -0,0 +1,10 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+Form, Window = uic.loadUiType("interface03.ui")
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+app.exec()
